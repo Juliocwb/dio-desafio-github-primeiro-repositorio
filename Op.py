0b17829b4e301529b165968ed5a7def7761b56b3
juliocwb <julio.cwb@gmail.com>
2022-09-26 13:00:00 -0300

    Aulaspandas

---
+++ Op.py
@@ -0,0 +1,12 @@
+#entrada = input().split()
+
+#distancia = int(entrada[0])
+#diametro1 = int(entrada[1])
+#diametro2 = int(entrada[2])
+
+#print("{:.2f}".format(distancia /(diametro1 + diametro2)))
+#valores = input().split() 
+
+#print("{:.2f}".format(int(valores[0]) / int(valores[1])))
+valores = input().split()
+print("{:.3f}".format((int(valores[0]) * int(valores[1]))/ 12))
