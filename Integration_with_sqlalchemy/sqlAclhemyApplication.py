60ba5709c07e4046645476492c340fd024065395
juliocwb <julio.cwb@gmail.com>
2022-11-04 17:45:16 -0300

    corregindo erros

--- Integration_with_sqlalchemy/sqlAclhemyApplication.py
+++ Integration_with_sqlalchemy/sqlAclhemyApplication.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column
+from sqlalchemy import Column, create_engine, inspect
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
@@ -10,24 +10,24 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = "user_acount"
+    __tablename__ = "user_account"
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
     fullname = Column(String)
 
     address = relationship(
-        "Address", back_populates="User", cascade= "all, delete-orphan"
+        "Address", back_populates="user", cascade= "all, delete-orphan"
     )
 
     def __repr__(self):
-        return f"User(id={self.id}, name={self.name}, fullname={self.fullname})"
+        return f"User (id={self.id}, name={self.name}, fullname={self.fullname})"
 
-class Adrress(Base):
+class Address(Base):
     __tablename__ = "address"
     id = Column(Integer, primary_key=True)
     email_address = Column(String(30),nullable=False)
-    user_id = Column(Integer, ForeignKey("User_account.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
 
 
     user = relationship("User", back_populates="address")
@@ -36,4 +36,12 @@ class Adrress(Base):
         return  f"Address (id={self.id}, email={self.email_address})"
 
 print(User.__tablename__)
-print(Adrress.__table__)
+print(Address.__table__)
+
+engine = create_engine("sqlite://")
+
+Base.metadata.create_all(engine)
+
+#print(engine.table_names())
+inspetor_engine = inspect(engine)
+print(inspetor_engine.get_table_names())
